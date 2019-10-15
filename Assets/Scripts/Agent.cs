de86e984b6428aa6d14f34ffcb10ffea34ac9842
carajo <joseomar9219@gmail.com>
2019-10-14 21:31:51 -0500

    p

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+	public Transform target;
+	public Transform target2;
+
+	void Start()
+	{
+		maxSpeed = 10f;
+		maxSteer = 0.5f;
+
+	//	target2 = GameObject.Find("Target2").transform;
+	}
+
+	void Update()
+	{
+		velocity += SteeringBehaviours.Arrive(this, target, 3);
+		velocity += SteeringBehaviours.Flee(this, target2, 1);
+		velocity += SteeringBehaviours.Separate(this, GameManager.agents, 2f);
+		transform.position += velocity * Time.deltaTime;
+	}
+}
